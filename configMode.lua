f2bba8292b126e169a31cd5f0e0878529c958c26
nouc <nouc23@gmail.com>
2017-02-05 13:21:27 +0100

    add http server and config form page

--- configMode.lua
+++ configMode.lua
@@ -13,3 +13,18 @@ wifi.ap.config(cfg)
  
 print("Starting AP with", cfg.ssid, cfg.pwd)
 wifi.setmode(wifi.SOFTAP)
+
+-- setup http
+srv=net.createServer(net.TCP)
+srv:listen(80,function(conn)
+  conn:on("receive",function(conn,payload)
+    print(payload)
+
+	if file.open("config.html") then
+    	conn:send(file.read())
+	  	file.close()
+	end
+
+  end)
+  conn:on("sent",function(conn) conn:close() end)
+end)
