f6de7ab450a04b06ea881bc27ae146e49bb09886
nouc <nouc23@gmail.com>
2017-02-05 12:51:53 +0100

    setup access point on config mode

--- configMode.lua
+++ configMode.lua
@@ -1,1 +1,15 @@
-print('in config mode')
+-- setup AP
+print("Setup Access Point")
+ipcfg = {}
+ipcfg.ip="192.168.1.1"
+ipcfg.netmask="255.255.255.0"
+ipcfg.gateway="192.168.1.1"
+wifi.ap.setip(ipcfg)
+ 
+cfg={}
+cfg.ssid="door-notifier"
+cfg.pwd="12345678"
+wifi.ap.config(cfg)
+ 
+print("Starting AP with", cfg.ssid, cfg.pwd)
+wifi.setmode(wifi.SOFTAP)
