a68d0478a769fb123d3ea19ead90bd1575d8d655
nouc <nouc23@gmail.com>
2017-02-14 20:14:49 +0100

    delay on start

--- init.lua
+++ init.lua
@@ -1,12 +1,16 @@
+TMR_ID_START = 0
+
 print('Starting...')
+tmr.alarm(TMR_ID_START, 3000, 1, function()
+    config_mode = true; -- @todo determinate on gpio
+    
+    if config_mode then
+        print('Starting config mode...')
+        dofile('setup-ap.lua')
+        dofile('config-server.lua')
+    else
+        print('Starting work mode...')
+        dofile('work-mode.lua')
+    end    
+end)
 
-config_mode = true; -- @todo determinate on gpio
-
-if config_mode then
-	print('Starting config mode...')
-	dofile('setup-ap.lua')
-	dofile('config-server.lua')
-else
-    print('Starting work mode...')
-    dofile('work-mode.lua')
-end
